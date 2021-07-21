a4426b083196b3ab07dfe8a76f24da859108ee4f
João Brito <joao@bitcapital.com.br>
2021-07-21 20:21:09 -0300

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+#![no_std]
+#![no_main]
+#![feature(asm,abi_x86_interrupt)]
+
+use core::panic::PanicInfo;
+
+use x86_64::registers::control::Cr3;
+
+mod vga_buffer;
+mod interrupts;
+mod gdt;
+mod serial;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+  println!("{}", info);
+  loop {
+    x86_64::instructions::hlt();
+  }
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+  // This is where the kernel code starts
+  println!("[ KERNEL START ] Started loading kernel...");
+
+  // Initiate GDT
+  gdt::init();
+
+  // Initiate interrupt handlers
+  interrupts::init_idt();
+  unsafe { interrupts::PICS.lock().initialize() };
+  x86_64::instructions::interrupts::enable();
+
+  loop {
+    x86_64::instructions::hlt();
+  }
+  // Simulate interrupt
+  // x86_64::instructions::interrupts::int3(); // new
+
+  // Simulate page fault
+  // unsafe {
+  //   *(0xdeadbeef as *mut u64) = 42;
+  // };
+  // fn stack_overflow() {
+  //   stack_overflow(); // for each recursion, the return address is pushed
+}
