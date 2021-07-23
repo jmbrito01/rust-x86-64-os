b5aaed3396148de8a4b99f182dba03ae67b7d9ab
João Marcelo Brito <jmbrito01@hotmail.com>
2021-07-23 12:40:06 -0300

    wip: feat: memory management

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,16 @@
 use core::panic::PanicInfo;
 
 use bootloader::BootInfo;
+use x86_64::{VirtAddr, structures::paging::{Translate}};
+
+//use crate::memory::active_level_4_table;
 bootloader::entry_point!(kernel_main);
 
 mod vga_buffer;
 mod interrupts;
 mod gdt;
 mod serial;
+mod memory;
 
 /// This function is called on panic.
 #[panic_handler]
@@ -21,7 +25,7 @@ fn panic(info: &PanicInfo) -> ! {
   }
 }
 
-fn kernel_main(boot_info:   &'static BootInfo) -> ! {
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
   // This is where the kernel code starts
   println!("[ KERNEL START ] Started loading kernel...");
 
@@ -32,6 +36,28 @@ fn kernel_main(boot_info:   &'static BootInfo) -> ! {
   interrupts::init_idt();
   unsafe { interrupts::PICS.lock().initialize() };
   x86_64::instructions::interrupts::enable();
+
+  let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+  // new: initialize a mapper
+  let mapper = unsafe { memory::init(phys_mem_offset) };
+  let addresses = [
+    // the identity-mapped vga buffer page
+    0xb8000,
+    // some code page
+    0x201008,
+    // some stack page
+    0x0100_0020_1a10,
+    // virtual address mapped to physical address 0
+    boot_info.physical_memory_offset,
+  ];
+
+
+
+  for &address in &addresses {
+    let virt = VirtAddr::new(address);
+    let phys = unsafe { mapper.translate_addr(virt) };
+    println!("{:?} -> {:?}", virt, phys);
+  }
 
   loop {
     x86_64::instructions::hlt();
